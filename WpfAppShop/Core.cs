0c85c81000294534545411d55bbe8b04815d3e13
Алексей Солдатов <alexey.soldatov.1997@gmail.com>
2026-02-02 22:29:25 +0300

    Подключена БД с помощью модели ado.net edm Добавлены ссылки на недостающие библиотеки Добавлен класс Core. Список MyCart для хранения корзины

---
+++ WpfAppShop/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppShop
+{
+    public static class Core
+    {
+        public static OnlineStoreDBEntities Context = new OnlineStoreDBEntities();
+
+        public static List<Products> MyCart = new List<Products>();
+    }
+}
