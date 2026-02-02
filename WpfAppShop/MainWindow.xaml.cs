ee0b7f8c09ac49dcf1ea22b2b64970b4a92adcab
Алексей Солдатов <alexey.soldatov.1997@gmail.com>
2026-02-02 22:32:18 +0300

    Сделано MainWindow. Оно служит контейнером. В нем есть Frame для переключения страниц и кнопка перехода в корзину. При запуске открываем страницу товаров

--- WpfAppShop/MainWindow.xaml.cs
+++ WpfAppShop/MainWindow.xaml.cs
@@ -15,14 +15,22 @@ using System.Windows.Shapes;
 
 namespace WpfAppShop
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new ProductPage());
+        }
+
+        private void BtnCatalog_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ProductPage());
+        }
+
+        private void BtnCart_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CartPage());
         }
     }
 }
